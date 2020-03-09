294329e82592435ee9f41db43d8961cae2f0501a
Wagner Truppel <trupwl@gmail.com>
2020-03-09 19:56:48 +0000

    Faster permutation implementation

--- Sources/Combinatorics/Permutations.swift
+++ Sources/Combinatorics/Permutations.swift
@@ -4,18 +4,56 @@ import Foundation
 
 extension Combinatorics {
 
-    // Todo: a more performant implementation.
-    
-    /// Computes all *permutations* of the input elements. The result is, therefore,
-    /// an array of length-`n` arrays, where `n` is the length of the input array.
-    /// The implementation aims to be as efficient as possible by allocating upfront
-    /// all the storage it needs.
+    /// Computes all *permutations* of the input elements, using
+    /// [Heap's algorithm](https://en.wikipedia.org/wiki/Heap%27s_algorithm).
+    /// The result is, naturally, an array of length-`n` arrays, where `n` is
+    /// the length of the input array. The implementation aims to be as efficient
+    /// as possible by allocating upfront all the storage it needs and by minimising
+    /// the number of swaps.
     ///
     /// There's also an asynchronous variant that is executed on a given dispatch queue
     /// and which provides its result on another queue (typically, these would be the global
     /// queue with a specified quality-of-service class and the main queue, respectively).
     ///
     public static func permutations<T>(of elements: ArraySlice<T>) -> [[T]] {
+
+        let n = elements.count
+        guard n > 0 else { return [] }
+        if n == 1 { return [ [elements[0]] ] }
+        if n == 2 { return [ [elements[0], elements[1]], [elements[1], elements[0]] ] }
+
+        let uResultCount: UInt = factorial(of: UInt(n))
+        let resultCount = Int(uResultCount)
+
+        // Allocate all the space needed, upfront
+        var res = [[T]](repeating: Array(elements), count: resultCount)
+        var c = [Int](repeating: 0, count: n)
+        var indices = [Int](0 ..< n)
+
+        var resIdx = 1
+        var i = 0
+        while i < n {
+            if c[i] < i {
+                if i % 2 == 0 {
+                    (indices[0], indices[i]) = (indices[i], indices[0])
+                } else {
+                    (indices[c[i]], indices[i]) = (indices[i], indices[c[i]])
+                }
+                for k in 0 ..< n { res[resIdx][k] = elements[indices[k]] }
+                resIdx += 1
+                c[i] += 1
+                i = 0
+            } else {
+                c[i] = 0
+                i += 1
+            }
+        }
+
+        return res
+
+    }
+
+    public static func permutations_old<T>(of elements: ArraySlice<T>) -> [[T]] {
         var res: [[T]] = []
         elements.enumerated().forEach { k, entry in
             let prefix = elements.prefix(upTo: k)
@@ -31,10 +69,12 @@ extension Combinatorics {
         return res
     }
 
-    /// Computes all *permutations* of the input elements. The result is, therefore,
-    /// an array of length-`n` arrays, where `n` is the length of the input array.
-    /// The implementation aims to be as efficient as possible by allocating upfront
-    /// all the storage it needs.
+    /// Computes all *permutations* of the input elements, using
+    /// [Heap's algorithm](https://en.wikipedia.org/wiki/Heap%27s_algorithm).
+    /// The result is, naturally, an array of length-`n` arrays, where `n` is
+    /// the length of the input array. The implementation aims to be as efficient
+    /// as possible by allocating upfront all the storage it needs and by minimising
+    /// the number of swaps.
     ///
     /// There's also an asynchronous variant that is executed on a given dispatch queue
     /// and which provides its result on another queue (typically, these would be the global
@@ -52,10 +92,12 @@ extension Combinatorics {
 
     public typealias PermutationsCompletionHandler<T> = ([[T]]) -> Void
 
-    /// Computes all *permutations* of the input elements. The result is, therefore,
-    /// an array of length-`n` arrays, where `n` is the length of the input array.
-    /// The implementation aims to be as efficient as possible by allocating upfront
-    /// all the storage it needs.
+    /// Computes all *permutations* of the input elements, using
+    /// [Heap's algorithm](https://en.wikipedia.org/wiki/Heap%27s_algorithm).
+    /// The result is, naturally, an array of length-`n` arrays, where `n` is
+    /// the length of the input array. The implementation aims to be as efficient
+    /// as possible by allocating upfront all the storage it needs and by minimising
+    /// the number of swaps.
     ///
     /// This asynchronous variant executes its code on a given dispatch queue (`executionQueue`)
     /// and provides its result on another queue (`receiverQueue`). Typically, these would be
@@ -78,10 +120,12 @@ extension Combinatorics {
         }
     }
 
-    /// Computes all *permutations* of the input elements. The result is, therefore,
-    /// an array of length-`n` arrays, where `n` is the length of the input array.
-    /// The implementation aims to be as efficient as possible by allocating upfront
-    /// all the storage it needs.
+    /// Computes all *permutations* of the input elements, using
+    /// [Heap's algorithm](https://en.wikipedia.org/wiki/Heap%27s_algorithm).
+    /// The result is, naturally, an array of length-`n` arrays, where `n` is
+    /// the length of the input array. The implementation aims to be as efficient
+    /// as possible by allocating upfront all the storage it needs and by minimising
+    /// the number of swaps.
     ///
     /// This asynchronous variant executes its code on a given dispatch queue (`executionQueue`)
     /// and provides its result on another queue (`receiverQueue`). Typically, these would be
--- Tests/CombinatoricsTests/PermutationsTests.swift
+++ Tests/CombinatoricsTests/PermutationsTests.swift
@@ -24,7 +24,7 @@ final class PermutationsTests: XCTestCase {
 
     func test_triple() {
         let exp = [
-            [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]
+            [1,2,3], [2,1,3], [3,1,2], [1,3,2], [2,3,1], [3,2,1]
         ]
         let res = Combinatorics.permutations(of: [1,2,3])
         XCTAssertTrue(res == exp)
@@ -32,10 +32,10 @@ final class PermutationsTests: XCTestCase {
 
     func test_quadra() {
         let exp = [
-            [1,2,3,4], [1,2,4,3], [1,3,2,4], [1,3,4,2], [1,4,2,3], [1,4,3,2],
-            [2,1,3,4], [2,1,4,3], [2,3,1,4], [2,3,4,1], [2,4,1,3], [2,4,3,1],
-            [3,1,2,4], [3,1,4,2], [3,2,1,4], [3,2,4,1], [3,4,1,2], [3,4,2,1],
-            [4,1,2,3], [4,1,3,2], [4,2,1,3], [4,2,3,1], [4,3,1,2], [4,3,2,1]
+            [1,2,3,4], [2,1,3,4], [3,1,2,4], [1,3,2,4], [2,3,1,4], [3,2,1,4],
+            [4,2,1,3], [2,4,1,3], [1,4,2,3], [4,1,2,3], [2,1,4,3], [1,2,4,3],
+            [1,3,4,2], [3,1,4,2], [4,1,3,2], [1,4,3,2], [3,4,1,2], [4,3,1,2],
+            [4,3,2,1], [3,4,2,1], [2,4,3,1], [4,2,3,1], [3,2,4,1], [2,3,4,1]
         ]
         let res = Combinatorics.permutations(of: [1,2,3,4])
         XCTAssertTrue(res == exp)
@@ -83,37 +83,29 @@ final class PermutationsTests: XCTestCase {
         }
     }
 
-    func test_triple_async() {
-        let a = [1,2,3]
-        let exp = [
-            [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]
-        ]
-        Combinatorics.permutations(
-            of: a,
-            executionQueue: DispatchQueue.global(qos: .background),
-            receiverQueue: DispatchQueue.main
-        ) { res in
-            XCTAssertTrue(Thread.isMainThread)
-            XCTAssertTrue(res == exp)
+    func test_perf() {
+        var start: Date
+        var end: Date
+        for n in 1 ... 10 {
+            let input = ArraySlice([Int](1 ... n))
+            start = Date()
+            _ = Combinatorics.permutations_old(of: input)
+            end = Date()
+            let oldDelta = end.timeIntervalSince(start)
+            start = Date()
+            _ = Combinatorics.permutations(of: input)
+            end = Date()
+            let newDelta = end.timeIntervalSince(start)
+            print("-----")
+            print("n = \(n)")
+            print("old = \(oldDelta)")
+            print("new = \(newDelta)")
+            let r = Double(Int(1000 * (oldDelta / newDelta))) / 1000
+            print("old is \(r) times slower than new")
+            let s = Double(Int(100000 * (oldDelta - newDelta) / newDelta)) / 1000
+            print("old is \(s) % slower than new")
         }
-    }
-
-    func test_quadra_async() {
-        let a = [1,2,3,4]
-        let exp = [
-            [1,2,3,4], [1,2,4,3], [1,3,2,4], [1,3,4,2], [1,4,2,3], [1,4,3,2],
-            [2,1,3,4], [2,1,4,3], [2,3,1,4], [2,3,4,1], [2,4,1,3], [2,4,3,1],
-            [3,1,2,4], [3,1,4,2], [3,2,1,4], [3,2,4,1], [3,4,1,2], [3,4,2,1],
-            [4,1,2,3], [4,1,3,2], [4,2,1,3], [4,2,3,1], [4,3,1,2], [4,3,2,1]
-        ]
-        Combinatorics.permutations(
-            of: a,
-            executionQueue: DispatchQueue.global(qos: .background),
-            receiverQueue: DispatchQueue.main
-        ) { res in
-            XCTAssertTrue(Thread.isMainThread)
-            XCTAssertTrue(res == exp)
-        }
+        print("-----")
     }
 
 }
